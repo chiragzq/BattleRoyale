d1deabe4064cff9ff0518a8f47c1a3e1d9267700
Aaron Lo <22aaronl@students.harker.org>
2019-05-06 21:39:40 -0700

    Allows Bullet to move
    Added the move() method which moves the bullet in its direction

--- Bullet.java
+++ Bullet.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * A Bullet the Player Fires
  * 
@@ -7,12 +7,13 @@
  */
 public class Bullet
 {
-    // instance variables - replace the example below with your own
     private int x;
     private int y;
     private int direction;
-    private final int SPEED = 10;
-    private final int DISTANCE = 10;
+    private int distance;
+    //How far the bullet goes
+    private static final int SPEED = 10;
+    private static final int DAMAGE = 10;
     
     /**
      * Bullet constructor
@@ -25,5 +26,39 @@ public class Bullet
         x = xLoc;
         y = yLoc;
         direction = direc;
+        
+        distance = 10;
+    }
+    
+    /**
+     * Moves the Bullet
+     * @return false if the bullet should not be moving anymore; true if it can
+     */
+    public boolean move()
+    {
+        if(direction == 0)
+            return false;
+        y += Math.sin(Math.toRadians(direction)) * SPEED;
+        x += Math.cos(Math.toRadians(direction)) * SPEED;
+        direction--;
+        return true;
+    }
+    
+    /**
+     * This gets the x
+     * @return the x Loc
+     */
+    public int getX()
+    {
+        return x;
+    }
+    
+    /**
+     * This gets the y location
+     * @return the y Loc
+     */
+    public int getY()
+    {
+        return y;
     }
 }
