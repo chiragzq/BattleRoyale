c6fe1b3496ee57980529ce82ca9f513f8f9ed2b8
Aaron Lo <22aaronl@students.harker.org>
2019-05-14 22:11:15 -0700

    Images
    Images for fist, pistol, rifle, and shotgun

--- Game.java
+++ Game.java
@@ -65,7 +65,7 @@ public class Game extends JComponent implements KeyListener, MouseListener {
         players = new HashMap<Integer, Player>();
         bullets = new HashMap<Integer, Bullet>();
 
-        network = new Network("http://localhost:3000", this, lock);
+        network = new Network("http://apcs-survivio.herokuapp.com", this, lock);
 
         new Timer().schedule(new TimerTask(){
                 @Override
